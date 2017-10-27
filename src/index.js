e8a53a533f7e29ef2194b9afe754867762b8f4ca
xsdlr <869412057@qq.com>
2017-10-27 17:13:07 +0800

    修复引用该库的三方库无法修改日志级别的错误

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import localStorage from './localStorage'
 import sessionStorage from './sessionStorage'
 import Logger from 'log4js-helper'
 
-Logger.setLevel(process.env.NODE_ENV === 'production' ? Logger.WARN : Logger.DEBUG)
 export {localStorage, sessionStorage}
 export default {
   localStorage,
